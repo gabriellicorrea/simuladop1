e5a06fa815bb3fd8fad9b0271eeabe367b6cfadd
EC2 Default User <ec2-user@ip-172-31-17-175.ec2.internal>
2019-09-25 17:24:36 +0000

    teste
    M-\
    
    ^X
    
    ^X

---
+++ eleicao.rb
@@ -0,0 +1,13 @@
+#Em uma eleicao parlamentar, ha tres partidos: Azul, Vermelho e Verde. Cada 
+#candidato possui um numero que o identifica, o partido, quantos votos recebeu,
+#e seu nome. A eleicao e composta pelo ano e por muitos candidatos.
+#Sua tarefa e implementar funcionalidades, usando a programacao orientada a 
+#objetos, que permitam:
+#(a) Mostrar o ano corrente da eleicao;
+#(b) Votar a partir de um numero;
+#(c) Mostrar votos de todos os candidatos;
+#(d) Mostrar votos de todos os partidos;
+
+class Oi
+    
+end
